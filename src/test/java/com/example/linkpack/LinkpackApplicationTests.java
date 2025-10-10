05c27317900ce637ef221f2c4a5323ac9abf977e
arshia <chillguyfromspace@gmail.com>
2025-10-10 10:29:35 +0330

    create test class

---
+++ src/test/java/com/example/linkpack/LinkpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
