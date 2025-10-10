0a8bf9530e12b3f451f96d8b3831a6341d7f0688
arshia <chillguyfromspace@gmail.com>
2025-10-10 10:31:46 +0330

    create linkpack application .java

---
+++ src/main/java/com/example/linkpack/LinkpackApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkpackApplication.class, args);
+	}
+
+}
