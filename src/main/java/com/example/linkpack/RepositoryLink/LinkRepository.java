5b32544d37e96e54155e9c4a11c90388727c3796
arshia <chillguyfromspace@gmail.com>
2025-10-22 20:41:40 +0330

    add two method in LinkRepository.java

--- src/main/java/com/example/linkpack/RepositoryLink/LinkRepository.java
+++ src/main/java/com/example/linkpack/RepositoryLink/LinkRepository.java
@@ -2,6 +2,7 @@ package com.example.linkpack.RepositoryLink;
 
 import com.example.linkpack.Models.LinkModelWithDataBase;
 import org.springframework.data.jpa.repository.JpaRepository;
+kk`import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,10 +12,13 @@ import java.util.Optional;
 @Repository
 public interface LinkRepository extends JpaRepository<LinkModelWithDataBase, Long> {
 
-Optional<LinkModelWithDataBase> findByShortCode(String shortCode);
+    Optional<LinkModelWithDataBase> findByOriginalUrl(String originalUrl);
 
-boolean existsByShortCode(String shortCode);
+    Optional<LinkModelWithDataBase> findByShortCode(String shortCode);
 
+    boolean existsByShortCode(String shortCode);
 
-
+    // برای پیدا کردن پرکلیک‌ترین لینک
+    @Query("SELECT l FROM LinkModelWithDataBase l ORDER BY l.clicks DESC LIMIT 1")
+    Optional<LinkModelWithDataBase> findTopByOrderByClicksDesc();
 }
