7866545074b0dccc1f535478abc1cc2f28fed7c2
arshia <chillguyfromspace@gmail.com>
2025-10-22 20:46:45 +0330

    just a test maybe wrong !

--- src/test/java/com/example/linkpack/LinkpackApplicationTests.java
+++ src/test/java/com/example/linkpack/LinkpackApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.linkpack;
-
+import com.example.linkpack.Services.UrlServiceWithDataBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootTest
+@ComponentScan(basePackages = "com.example.linkpack")
 class LinkpackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	public void testURl(){
+//		UrlServiceWithDataBase test = new UrlServiceWithDataBase();
+//		String result = test.ShortUrl("https://grok.com/c/18deaa0e-f24f-4cfe-8a34-f6492d7e00a4");
+//		System.out.println(result);
+//	}
+//
+//
+
+
 
 }
