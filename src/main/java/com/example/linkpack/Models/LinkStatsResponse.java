7ea9e7648d2d2fef3fef054271e3a3ddee9fbd2f
arshia <chillguyfromspace@gmail.com>
2025-11-01 21:43:50 +0330

    add constructor

--- src/main/java/com/example/linkpack/Models/LinkStatsResponse.java
+++ src/main/java/com/example/linkpack/Models/LinkStatsResponse.java
@@ -18,10 +18,26 @@ public class LinkStatsResponse {
         this.shortUrl = shortUrl;
     }
 
+    public LinkStatsResponse(LinkModelWithDataBase link, String shortUrl) {
+        this.originalUrl = link.getOriginalUrl();
+        this.shortCode = link.getshortCode();
+        this.shortUrl = shortUrl + "/" + link.getshortCode();
+        this.clicks = link.getClicks();
+        this.timestamp = link.getTimestamp();
+    }
+
+    public LinkStatsResponse(String originalUrl, String shortcode, String shorturl) {
+    }
+
     // Getters
-    public String getShortCode() { return shortCode; }
-    public String getOriginalUrl() { return originalUrl; }
-    public int getClicks() { return clicks; }
-    public LocalDateTime getCreatedAt() { return timestamp; }
-    public String getShortUrl() { return shortUrl; }
+    public String getShortCode() {
+        return shortCode; }
+    public String getOriginalUrl() {
+        return originalUrl; }
+    public int getClicks() {
+        return clicks; }
+    public LocalDateTime getCreatedAt() {
+        return timestamp; }
+    public String getShortUrl() {
+        return shortUrl; }
 }
