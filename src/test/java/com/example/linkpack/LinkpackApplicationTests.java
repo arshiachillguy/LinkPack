bcf989de7fab6633caa9e590fbc7d4d21b49bf76
arshia <chillguyfromspace@gmail.com>
2025-12-03 11:05:42 +0330

    nothing

--- src/test/java/com/example/linkpack/LinkpackApplicationTests.java
+++ src/test/java/com/example/linkpack/LinkpackApplicationTests.java
@@ -5,20 +5,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @Configuration
 @SpringBootTest
 @ComponentScan(basePackages = "com.example.linkpack")
 class LinkpackApplicationTests {
 
-//	@Test
-//	public void testURl(){
-//		UrlServiceWithDataBase test = new UrlServiceWithDataBase();
-//		String result = test.ShortUrl("https://grok.com/c/18deaa0e-f24f-4cfe-8a34-f6492d7e00a4");
-//		System.out.println(result);
-//	}
-//
-//
+    @Test
+    public void generateshortcode(){
+        UrlServiceWithDataBase test = new UrlServiceWithDataBase();
 
+        String resulte = test.createShortLink("www.google.com");
 
-
+        assertNotNull(resulte);
+    }
 }
