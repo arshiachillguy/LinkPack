3ab91dea3f58b1ac960ea48af1f3bd66ed2e6234
arshia <chillguyfromspace@gmail.com>
2026-01-05 10:55:16 +0330

    change routes and add protected and public api's

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,9 +1,12 @@
 import { createContext, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import History from "./pages/History";
 import Profile from "./pages/Profile";
 import Homepage from "./pages/Homepage";
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import Login from "./pages/Login";
 
 export const UIContext = createContext<any>(null);
 
@@ -13,14 +16,20 @@ export default function App() {
 
   return (
     <UIContext.Provider value={{ theme, setTheme, fontSize, setFontSize }}>
-      {/* <BrowserRouter> */}
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      {/* </BrowserRouter> */}
+      <AuthProvider>
+        <Routes>
+          {/* PUBLIC */}
+          <Route path="/login" element={<Login />} />
+
+          {/* PROTECTED */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/about" element={<About />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </UIContext.Provider>
   );
 }
